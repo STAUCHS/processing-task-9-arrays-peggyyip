ed26e56e2ea019b05bea6cbd3596f3a67136ca46
yourGithubUsername <yourGithubAccountEmail>
2024-05-19 01:30:55 -0400

    updated

--- Sketch.java
+++ Sketch.java
@@ -5,11 +5,26 @@ public class Sketch extends PApplet {
   // Related arrays for the (x, y) coordinate of the snowflakes
   float[] snowX = new float[42];
   float[] snowY = new float[42];
+
+  // Determining size of snowflakes
   int snowDiameter = 10;
+
+  // Key values
   boolean upPressed = false;
   boolean downPressed = false;
+  boolean wPressed = false;
+  boolean sPressed = false;
+  boolean aPressed = false;
+  boolean dPressed = false;
+
+  // Initializing speed variables
   float fltCircleX = 0;
   float fltCircleY = 0;
+  float fltPlayerX = 0;
+  float fltPlayerY = 0;
+
+  // Defining variables
+  int playerLives = 3;
 
   public void settings() {
     size(400, 400);
@@ -31,24 +46,46 @@ public class Sketch extends PApplet {
     // Draw snow
     snow();
     
+    if(key == 'w' || key == 'W' || key == 's' || key == 'S' || key == 'a' || key == 'A' || key == 'd' || key == 'D' || keyCode == DOWN || keyCode == UP) {
+      movingPlayer();
+    }
   }
-      private void movingSun() {
-        // Add player logo
-        fill(128, 191, 242);
-        circle(fltCircleX, fltCircleY, 80);
-        if (key == 'w' || key == 'W' ) {
-          fltCircleY--;
-        }
-        else if (key == 's' || key == 'S' ) {
-          fltCircleY++;
-        }
-        else if (key == 'a' || key == 'A') {
-          fltCircleX--;
-        }
-        else if (key == 'd' || key == 'D') {
-          fltCircleX++;
-        }
+    private void movingPlayer() {
+      if (wPressed) {
+        fltPlayerY -= 2;
       }
+      else if (sPressed ) {
+        fltPlayerY += 2;
+      }
+      else if (aPressed) {
+        fltPlayerX -= 2;
+      }
+      else if (dPressed) {
+        fltPlayerX += 2;
+      }
+
+      // Add player representative
+      fill(128, 191, 242);
+      circle(fltPlayerX, fltPlayerY, 20);
+
+      // Create boundaries
+      fltPlayerX = Math.max(20, Math.min(fltPlayerX, width - 20));
+      fltPlayerY = Math.max(20, Math.min(fltPlayerY, height - 20));
+
+      // Check for collision with snowflakes
+      for (int i = 0; i < 42; i++) {
+      if (snowY[i] >= height - 10 && Math.abs(snowX[i] - fltPlayerX) < 20 + 10) {
+          playerLives--;
+          if (playerLives == 0) {
+              background(255);
+              System.out.println("Game Over!");
+          }
+      }
+    }
+      if (playerLives == 2 || playerLives == 1) {
+        
+      }
+  }
   
   public void snow(){
     fill(255);
@@ -57,12 +94,13 @@ public class Sketch extends PApplet {
 
       snowY[i] += 2;
 
-      // Reset snowflakes
-      if (snowY[i] > height) {
-        snowY[i] = 0;
+    // Reset snowflakes
+    if (snowY[i] > height) {
+      snowY[i] = 0;
       }
     }
-      // Make snow fall faster
+
+    // Make snow fall faster
     if(downPressed) {
       for(int i = 0; i < snowX.length; i++) {
         circle(snowX[i], snowY[i], snowDiameter);
@@ -83,15 +121,41 @@ public class Sketch extends PApplet {
     else if (keyCode == UP) {
       upPressed = true;
     }
+    if (key == 'w' || key == 'W') {
+      wPressed = true;
+    }
+    else if (key == 's' || key == 'S') {
+      sPressed = true;
+    }
+    else if (key == 'a' || key == 'A') {
+      aPressed = true;
+    }
+    else if (key == 'd' || key == 'D') {
+      dPressed = true;
+    }
   }
+  
   public void keyReleased() {
     if (keyCode == DOWN) {
       downPressed = false;
      }
-     else if (keyCode == UP) {
+    else if (keyCode == UP) {
       upPressed = false;
-      }
     }
+
+    if (key == 'w' || key == 'W') {
+      wPressed = false;
+    }
+    else if (key == 's' || key == 'S') {
+      sPressed = false;
+    }
+    else if (key == 'a' || key == 'A') {
+      aPressed = false;
+    }
+    else if (key == 'd' || key == 'D') {
+      dPressed = false;
+    }
+  }
 
     
 }
